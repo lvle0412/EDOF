30833a6456ae9605b677b6b8f7eb56241fda3099
Andrew Leifer <leifer@princeton.edu>
2013-04-15 14:50:16 -0400

    whoa snap! mingw-w64 is compiling simple opencv code.

--- test.c
+++ test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "highgui.h"
+#include "cxcore.h"
+#include "cv.h"
 
 int main(){
-printf("Hello World.\n");
+printf("Hello World.\nWait\n");
+cvNamedWindow("RectWorm",CV_WINDOW_AUTOSIZE);
+cvWaitKey(0);
+cvWaitKey(500);
+printf("Done.\n");
 return 0;
 }

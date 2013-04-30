6f82e09ba48d1c7b44c713ad502e8c4ee0e1f700
Andrew Leifer <leifer@princeton.edu>
2013-04-30 11:28:22 -0400

    plucked annotate.py from dualMag branch for python 3 compatability

--- bin/annotate.py
+++ bin/annotate.py
@@ -40,13 +40,13 @@ parser.add_option("-u", "--user", action="store", type="string", dest="user", de
 
 
 if len(args) !=2:
-	print 'Error: incorrect number of arguments'	
+	print('Error: incorrect number of arguments')	
 	parser.print_help()
 	sys.exit(2)
 
 
 #Main 
-print 'Welcome to the Optical Mind Control annotation system.'
+print('Welcome to the Optical Mind Control annotation system.')
 
 
 
@@ -61,7 +61,7 @@ prevFrame=''
 
 # Main Loop
 while (s!='q'): #While the user doesn't quit
-	s=raw_input('Enter annotation (q to quit):\n')
+	s=input('Enter annotation (q to quit):\n')
 	if (s=='q'):
 		continue
 	
@@ -72,7 +72,7 @@ while (s!='q'): #While the user doesn't quit
 	try:
 		r=open(recentframefile,'r')
 	except:
-		print '\tError! ' +recentframefile + ' not found!\n'
+		print('\tError! ' +recentframefile + ' not found!\n')
 		continue
 
 
@@ -85,7 +85,7 @@ while (s!='q'): #While the user doesn't quit
 	# And the third line is the GIT SHA1 version hash
 	# And the fourth is an integer value 0 or 1 specifiying whether the DLP is on or not
 	if len(frameInfo) != 4: 
-		print '\tError! ' +recentframefile + ' does not have expected format!\n'
+		print('\tError! ' +recentframefile + ' does not have expected format!\n')
 		continue
 
 	if ( (prevFrame!=frameInfo[0].strip()) ):
@@ -112,16 +112,16 @@ while (s!='q'): #While the user doesn't quit
 	#If the frame number has incremented since last time 
 	if ( newFrame==True ):
 		out.write('\t\t- Frame: ' + frameInfo[0].strip()+ '\n\t\t  Description: "'+s+'"\n')
-		print '\tFrame ' +frameInfo[0].strip() +  ' recorded.'
+		print('\tFrame ' +frameInfo[0].strip() +  ' recorded.')
 
 	else:
 		if (firstGenAnnote==True):
 			out.write('\tOverview:\n')
 			firstGenAnnote=False
 		out.write('\t\t- Description: "'+s+'"\n')
-		print 'Experiment overview recorded.'
+		print('Experiment overview recorded.')
 	out.close()
 
 
-print "Good bye!"
+print("Good bye!")
 

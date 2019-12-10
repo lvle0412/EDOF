98bbbafd158c3e5851ef376367097e92e457cc2f
xutq <xutq@hotmail.com>
2019-12-10 10:49:05 +0800

    make colbert read mat successfully

--- MyLibs/Talk2Matlab.c
+++ MyLibs/Talk2Matlab.c
@@ -278,6 +278,46 @@ int LoadMatFileData(double *vec[], int *vectorsize, int *numberofvectors, const 
   	}
   mxFree(dir);
 
+    /* In order to use matGetNextXXX correctly, reopen file to read in headers. */
+  if (matClose(pmat) != 0) {
+    printf("Error closing file %s\n",file);
+    return -1;
+  }
+  pmat = matOpen(file, "r");
+  if (pmat == NULL) {
+    printf("Error reopening file %s\n", file);
+    return -1;
+  }
+
+  /* Get headers of all variables */
+  printf("\nExamining the header for each variable:\n");
+  for (i=0; i < ndir; i++) {
+    pa = matGetNextVariableInfo(pmat, &name);
+    if (pa == NULL) {
+	printf("Error reading in file %s\n", file);
+	return -1;
+    }
+    /* Diagnose header pa */
+    printf("According to its header, array %s has %d dimensions\n",
+	   name, mxGetNumberOfDimensions(pa));
+    if (mxIsFromGlobalWS(pa))
+      printf("  and was a global variable when saved\n");
+    else
+      printf("  and was a local variable when saved\n");
+    mxDestroyArray(pa);
+  }
+
+  /* Reopen file to read in actual arrays. */
+  if (matClose(pmat) != 0) {
+    printf("Error closing file %s\n",file);
+    return -1;
+  }
+  pmat = matOpen(file, "r");
+  if (pmat == NULL) {
+    printf("Error reopening file %s\n", file);
+    return -1;
+  }
+
   /* Read in each array. */
   printf("\nReading in the actual array contents:\n");
   for (i=0; i<ndir; i++) {
